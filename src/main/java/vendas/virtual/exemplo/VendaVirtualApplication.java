0b4c3a4c86f96b436986cf6e3cdfb0c5de22f47d
ThalisonCarlos <thalisonccb@hotmail.com>
2020-09-19 18:18:12 -0300

    inital commit

---
+++ src/main/java/vendas/virtual/exemplo/VendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package vendas.virtual.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaVirtualApplication.class, args);
+	}
+
+}
